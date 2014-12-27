97e609aef0fe7632ba84f0279ce1231c4573bfa7
Jamie Tanna <jamie@jamietanna.co.uk>
2014-12-27 00:15:41 +0000

    Added Baskerville's example BSPWM setup
    Affects: [BSPWM] [Bar]

---
+++ bspwm/global/etc/profile.d/bspwm_panel.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export PATH="$PATH:/usr/local/bin/panel"
+
+export PANEL_FIFO=/tmp/panel-fifo
+export PANEL_HEIGHT=24
+export PANEL_FONT_FAMILY="-*-terminus-medium-r-normal-*-12-*-*-*-c-*-*-1"
