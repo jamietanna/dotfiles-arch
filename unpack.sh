ee4b1069596a8738034949ac8a007c85b3fa6e23
Jamie Tanna <jamie@jamietanna.co.uk>
2015-01-10 23:56:14 +0000

    Replace unpack script with Coreutils-based one
    Instead of using GNU Stow, we'll use one that revolves around just
    coreutils - which means it's going to be much more portable and easy to
    use.
    
    Affects: [Unpack]

--- unpack.sh
+++ unpack.sh
@@ -1,44 +1,82 @@
 #!/bin/bash
 
-newdot () {
-	mkdir -p $1/{home,global}
-}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-err () {
-	echo -e "\033[91m$@\033[0m"
+cmd () {
+	echo "CMD: $2"
+	if [ "$1" == "home" ];
+	then
+		$2
+	else
+		sudo $2
+	fi
 }
 
 unpack () {
-	# unpack dir isGlobal
-	if [ -d "$1" ];
-	then
-		dir_old=$(pwd)
-		cd "$1"
-		if ! $2;
+	# PKG home|global
+	for f in $(find "$1/$2" -type d);
+	do
+		full_path="$(readlink -f "$f")"
+		path_create="${full_path//$DIR\/$1/}"
+
+		case "$2" in
+			home )
+			OUT=$HOME
+			path_create_final="${path_create//$1\/home/}"
+			;;
+			global )
+			OUT=
+			path_create_final="${path_create//$1\/global/}"
+			;;
+			* )
+			echo "ERR"
+			exit 1
+			;;
+		esac
+		path_split=(${path_create//\// })
+
+		[ -z "$OUT$path_create_final" ] && continue
+
+		cmd "$2" "mkdir -p $OUT$path_create_final"
+	done
+
+	for f in $(find "$1/$2" -type f);
+	do
+		full_path="$(readlink -f "$f")"
+		path_create="${full_path//$DIR\/$1/}"
+
+		case "$2" in
+			home )
+			OUT=$HOME
+			path_create_final="${path_create//$1\/home/}"
+			;;
+			global )
+			OUT=
+			path_create_final="${path_create//$1\/global/}"
+			;;
+			* )
+			echo "ERR"
+			exit 1
+			;;
+		esac
+		path_split=(${path_create//\// })
+
+		if [ -e "$OUT$path_create_final" ];
 		then
-			stow -t "$HOME" . 
-		else
-			sudo stow -t / .
+			echo "$OUT$path_create_final already exists"
+			continue
 		fi
-		cd "$dir_old"
-	fi
+
+		cmd "$2" "ln -s $full_path $OUT$path_create_final"
+	done
 }
 
-if ! which stow > /dev/null 2>&1;
+if [ -z "$1" ];
 then
-	err "Error: GNU stow is not installed, cannot continue"
+	echo "ERROR: Please enter name of package to install"
 	exit 1
 fi
 
-# TODO: need to only check for $1's from the executing dir (SO 59895)
-while test ${#} -gt 0
-do
-	if [ ! -d "$1" ];
-	then
-		err "$1 does not exist, skipping..."
-	else
-		unpack "$1/home" false
-		unpack "$1/global" true 
-	fi
-	shift
-done
+[ -d "$DIR/$1/home" ] && unpack "$DIR/$1" "home"
+	# this one is sudo !!
+[ -d "$DIR/$1/global" ] && unpack "$DIR/$1" "global"
