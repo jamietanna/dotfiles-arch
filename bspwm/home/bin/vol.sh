25c3c3467646c4cd09a06ed29910f72c4c8efa0b
Jamie Tanna <jamie@jamietanna.co.uk>
2015-02-12 23:50:07 +0000

    [SCRIPTS] Deal with Arch naming convention for ALSA devices

--- bspwm/home/bin/vol.sh
+++ bspwm/home/bin/vol.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+DEVICE=IEC958
+
 get_volume () {
 	# http://unix.stackexchange.com/a/89583
-	VOL=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master))
-	SOUND_ON=$(awk -F"[][]" '/dB/ { print $6 }' <(amixer sget Master))
+	VOL=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget $DEVICE))
+	SOUND_ON=$(awk -F"[][]" '/dB/ { print $6 }' <(amixer sget $DEVICE))
 	if [ "$SOUND_ON" == "off" ];
 	then
 		VOL=0
@@ -13,13 +15,13 @@ get_volume () {
 
 case "$1" in
 	up )
-		amixer -q sset Master 5%+
+		amixer -q sset $DEVICE 5%+
 		;;
 	down )
-		amixer -q sset Master 5%-
+		amixer -q sset $DEVICE 5%-
 		;;
 	toggle )
-		amixer -q sset Master toggle
+		amixer -q sset $DEVICE toggle
 		;;
 	get )
 		get_volume
