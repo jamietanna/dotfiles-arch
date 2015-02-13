c18f9d3363118ce96da89dd50e5822549e7f5361
Jamie Tanna <jamie@jamietanna.co.uk>
2015-02-13 16:21:50 +0000

    [BSPWM] Added support for HiDPI screen

--- bspwm/global/etc/profile.d/bspwm_panel.sh
+++ bspwm/global/etc/profile.d/bspwm_panel.sh
@@ -3,4 +3,4 @@ export PATH="$PATH:/usr/local/bin/panel"
 
 export PANEL_FIFO=/tmp/panel-fifo
 export PANEL_HEIGHT=24
-export PANEL_FONT_FAMILY="-*-terminus-medium-r-normal-*-12-*-*-*-c-*-*-1"
+export PANEL_FONT_FAMILY="-*-terminus-medium-r-normal-*-22-*-*-*-c-*-*-1"
