61a956864dbac6212afb3412b3de6c8ffe2a2de5
Jamie Tanna <jamie@jamietanna.co.uk>
2015-03-11 21:15:38 +0000

    Added YouCompleteMe config and plugin install
    Added a config mainly stolen from [`mblum` on Bitbucket]
    (https://bitbucket.org/mblum/libgp/src/2537ea7329ef/.ycm_extra_conf.py),
    and a few basic settings to get started with it, and an install script.
    
    Affects: [Vim]

--- vim/install.post.sh
+++ vim/install.post.sh
@@ -12,3 +12,11 @@ git clone https://github.com/wincent/command-t
 	make
 )
 
+echo -e "\033[33mWARNING: This next step will max out your CPU while compiling. Please ^C and comment out the following steps if you wish.\033[0m"
+read
+sudo pacman -S clang cmake
+git clone https://github.com/Valloric/YouCompleteMe
+(
+	cd YouCompleteMe
+	./install.sh --clang-completer --system-libclang
+)
