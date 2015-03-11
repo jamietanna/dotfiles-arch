d2aa884df3e8abb4ae55a2a2972a054e0602ca82
Jamie Tanna <jamie@jamietanna.co.uk>
2015-03-11 11:58:34 +0000

    Update bar when controlling our music via key combos
    Add a central script, `mpc.sh` which will update the bar status to
    reflect what's just been pressed, instead of waiting for the loop
    inside our bar script to do it (which takes time)
    
    Affects: [BSPWM] [LemonBar]

---
+++ bspwm/home/bin/mpc.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+current_status () {
+	echo -n "M"
+
+	mpc_status=$(mpc status)
+
+	if echo $mpc_status | grep "playing" 2>&1 > /dev/null;
+	then
+		echo -n "pl"
+	elif echo $mpc_status | grep "paused" 2>&1 > /dev/null;
+	then
+		echo -n "pa"
+	else
+		echo -n "st"
+		echo
+	fi
+
+	mpc current
+}
+
+case $1 in
+	"toggle"|"play"|"pause"|"prev"|"next"|"status"|"current" )
+		mpc $1 >/dev/null 2>&1
+		current_status > "$PANEL_FIFO"
+	;;
+esac
