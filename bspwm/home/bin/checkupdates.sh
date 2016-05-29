af8276e1cac943970cb776abd82d816fda9a82e8
Jamie Tanna <jamie@jamietanna.co.uk>
2016-05-29 21:36:48 +0100

    Display updates since last run more noticeably
    Add a cache of the last run of `checkupdates`, which will allow us to more
    visibly indicate whether a package has been added since the last run.

--- bspwm/home/bin/checkupdates.sh
+++ bspwm/home/bin/checkupdates.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+# TODO rotate _some number_ of entries to make sure we don't lose information if we miss the notification
+# TODO show 'new' updates first, then have the 'old' ones? then it reduces cognitive load, and makes it much easier to see how many are new since the last update
+
+CACHED_UPDATE_LIST=/var/run/user/$(id -u)/checkupdates.cache
+MAXLINES=1000
+
 if ! curl -s "http://google.com/" 2>&1 > /dev/null;
 then
 	notify-send "No network" "Cannot check for updates as there is no network"
 	exit 1
 fi
+
+# prevent diff errors by creating an empty CACHED_UPDATE_LIST if one does not
+# already exist
+[[ ! -e "$CACHED_UPDATE_LIST" ]] && touch "$CACHED_UPDATE_LIST"
 
 updates="$(checkupdates)"
 update_count=$(echo "$updates" | wc -l)
@@ -25,8 +35,28 @@ fi
 
 echo "$out_str" > $PANEL_FIFO
 
-if [[ "$1" == "notify" ]];
+update_diffs="$(diff -u$MAXLINES $CACHED_UPDATE_LIST <(echo "$updates"))"
+# remove the diff headers
+update_diffs="$(echo "$update_diffs" | tail -n+4)"
+# ignore any removed entries
+update_diffs="$(echo "$update_diffs" | grep --invert-match '^-')"
+# remove the leading space from our diff
+update_diffs="$(echo "$update_diffs" | sed 's/^ \(.*\)$/\1/')"
+# make new entries bold
+update_diffs="$(echo "$update_diffs" | sed 's/^+\(.*\)$/<b>\1<\/b>/')"
+
+# if we have specific diffs, display them, otherwise just list all the updates
+if [[ -n "$update_diffs" ]];
 then
-	notify-send -u "$urgency" "$update_count Updates" "$updates"
+	updates_out="$update_diffs"
+else
+	updates_out="$(cat $CACHED_UPDATE_LIST)"
 fi
 
+if [[ "$1" == "notify" ]];
+then
+	notify-send -u "$urgency" "$update_count Updates" "$updates_out"
+fi
+
+# update our cached updates list
+echo "$updates" > $CACHED_UPDATE_LIST
