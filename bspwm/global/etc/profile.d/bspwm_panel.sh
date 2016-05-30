167c29126e8187b9177933ac4b83bcd38f2bfbae
Jamie Tanna <jamie@jamietanna.co.uk>
2016-05-30 15:39:02 +0100

    Source local panel profile if it exists

--- bspwm/global/etc/profile.d/bspwm_panel.sh
+++ bspwm/global/etc/profile.d/bspwm_panel.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+PANEL_LOCAL_FILE="/etc/profile.d/bspwm_panel.sh.local"
 export PATH="$PATH:/usr/local/bin/panel"
 
 export PANEL_FIFO=/tmp/panel-fifo
 export PANEL_HEIGHT=24
 export PANEL_FONT_FAMILY="LiberationMono:size=11"
+
+if [[ -e "$PANEL_LOCAL_FILE" ]];
+then
+	. "$PANEL_LOCAL_FILE"
+fi
