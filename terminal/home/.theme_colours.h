95a833c41c26a86313310425b2fcb045e2d550cc
Jamie Tanna <jamie@jamietanna.co.uk>
2016-06-04 22:55:41 +0100

    Move system colours to a separate file
    To make it easier for other applications to easily parse our
    colourscheme, move it into a separate file that can then be accessed
    separately.

---
+++ terminal/home/.theme_colours.h
@@ -0,0 +1,46 @@
+// vim: foldmethod=marker:foldlevel=0 textwidth=80 wrap
+// ----------------------------------------------------------------------------
+// .theme_colors.h
+//	The colourscheme defined for the system, in a single file that can be easily
+//	accessed and parsed by other applications
+//	----------------------------------------------------------------------------
+
+// Based off morhetz/gruvbox-contrib
+
+#define col_bg                  #282828
+#define col_fg                  #ebdbb2
+#define col_black1              #282828
+#define col_black2              #927374
+#define col_red1                #cc241d
+#define col_red2                #fb4934
+#define col_green1              #98971a
+#define col_green2              #b8bb26
+#define col_yellow1             #d79921
+#define col_yellow2             #fabd2f
+#define col_blue1               #458588
+#define col_blue2               #83a598
+#define col_magenta1            #b16286
+#define col_magenta2            #d3869b
+#define col_cyan1               #689d6a
+#define col_cyan2               #8ec07c
+#define col_white1              #a89984
+#define col_white2              #ebdbb2
+#define col_white_true          #FFFFFF
+
+#define col_black               col_black1
+#define col_red                 col_red1
+#define col_green               col_green1
+#define col_yellow              col_yellow1
+#define col_blue                col_blue1
+#define col_magenta             col_magenta1
+#define col_cyan                col_cyan1
+#define col_white               col_white1
+
+#define col_black_dark          col_black2
+#define col_red_dark            col_red2
+#define col_green_dark          col_green2
+#define col_yellow_dark         col_yellow2
+#define col_blue_dark           col_blue2
+#define col_magenta_dark        col_magenta2
+#define col_cyan_dark           col_cyan2
+#define col_white_dark          col_white2
