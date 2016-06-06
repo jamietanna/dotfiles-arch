822e519a7e0380aa160b100c6b774e018001ad25
Jamie Tanna <jamie@jamietanna.co.uk>
2016-06-06 11:56:03 +0100

    Refactor network display in the panel
    It's more useful to be able to see at-a-glance, a per-interface status,
    rather than a 'global' status as before. This will additionally let me
    see the IPv4 address of an interface more easily, making it useful when
    i.e. trying to SSH between machines, or debug networking.

--- bspwm/home/bin/notify_network.sh
+++ bspwm/home/bin/notify_network.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
-tag=""
+DELIM=';'
+
+function ip_from_iface() {
+	# http://unix.stackexchange.com/a/87470
+	ip addr show $1 | grep -Po 'inet \K[\d.]+'
+}
+
+function get_iface_status() {
+	# $1: /sys/class/net/...
+	iface_path="$1"
+	local iface=$(basename $iface_path)
+	local outputstr=""
+	local tag=""
+	local ip=""
+
+	local operstate=$(cat $iface_path/operstate)
+	if [ "$operstate" == "up" ];
+	then
+		ip="$(ip_from_iface "$iface")"
+		tag="C"
+	else
+		tag="D"
+	fi
+
+	# a triple, _without_ brackets; they complicate the parsing process and
+	# don't really add anything
+	echo -n "${tag},${iface},${ip}"
+}
+
 output=""
 for iface_path in /sys/class/net/*;
 do
@@ -10,23 +38,6 @@ do
 		continue
 	fi
 
-	if [ -n "$output" ];
-	then
-		output="$output $iface"
-	else
-		output="$output$iface"
-	fi
-
-	# if a single connection is valid, that's enough
-	[ "$tag" != "" ] && continue
-
-	# use sysfs' information to find out whether we're connected or not
-	operstate=$(cat $iface_path/operstate)
-	if [ "$operstate" == "up" ];
-	then
-		tag="XC"
-	else
-		tag="XD"
-	fi
+	output+="$(get_iface_status $iface_path)${DELIM}"
 done
-echo "$tag$output"
+echo "X$output"
