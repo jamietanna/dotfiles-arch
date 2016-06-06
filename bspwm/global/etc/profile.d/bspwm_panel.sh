952a9f23e1704f74f79e9c4f4d454fc6eabba577
Jamie Tanna <jamie@jamietanna.co.uk>
2016-06-06 21:35:11 +0100

    Add default panel underline height

--- bspwm/global/etc/profile.d/bspwm_panel.sh
+++ bspwm/global/etc/profile.d/bspwm_panel.sh
@@ -5,6 +5,7 @@ export PATH="$PATH:/usr/local/bin/panel"
 export PANEL_FIFO=/tmp/panel-fifo
 export PANEL_HEIGHT=24
 export PANEL_FONT_FAMILY="LiberationMono:size=11"
+export PANEL_UNDERLINE_HEIGHT=3
 
 if [[ -e "$PANEL_LOCAL_FILE" ]];
 then
