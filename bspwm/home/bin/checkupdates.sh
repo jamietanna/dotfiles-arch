1323527c8b773c697592312910c2ffb16bf28d12
Jamie Tanna <jamie@jamietanna.co.uk>
2016-06-30 15:23:57 +0100

    checkupdates: Add AUR package checks
    Closes #33

--- bspwm/home/bin/checkupdates.sh
+++ bspwm/home/bin/checkupdates.sh
@@ -3,7 +3,8 @@
 # TODO rotate _some number_ of entries to make sure we don't lose information if we miss the notification
 # TODO show 'new' updates first, then have the 'old' ones? then it reduces cognitive load, and makes it much easier to see how many are new since the last update
 
-CACHED_UPDATE_LIST=/var/run/user/$(id -u)/checkupdates.cache
+CACHED_CORE_UPDATE_LIST=/var/run/user/$(id -u)/checkupdates.cache
+CACHED_AUR_UPDATE_LIST=/var/run/user/$(id -u)/checkupdates.aur.cache
 MAXLINES=1000
 
 if ! curl -s "http://google.com/" 2>&1 > /dev/null;
@@ -14,52 +15,81 @@ fi
 
 # prevent diff errors by creating an empty CACHED_UPDATE_LIST if one does not
 # already exist
-[[ ! -e "$CACHED_UPDATE_LIST" ]] && touch "$CACHED_UPDATE_LIST"
+[[ ! -e "$CACHED_CORE_UPDATE_LIST" ]] && touch "$CACHED_CORE_UPDATE_LIST"
+[[ ! -e "$CACHED_AUR_UPDATE_LIST" ]] && touch "$CACHED_AUR_UPDATE_LIST"
 
-updates="$(checkupdates)"
-update_count=$(echo "$updates" | wc -l)
+core_updates="$(checkupdates)"
+core_update_count=$(echo "$core_updates" | wc -l)
+# cower returns a non-zero status code if there are updates
+aur_updates="$(cower -u || true)"
+aur_updates="${aur_updates//:: /}"
+aur_update_count=$(echo "$aur_updates" | wc -l)
+
+total_update_count="$((core_update_count + aur_update_count))"
+
 out_str="P"
 urgency="normal"
-if [[ "$update_count" -eq "0" || -z "$updates" ]];
+if [[ "$total_update_count" -eq "0" || (-z "$core_updates" && -z "$aur_updates") ]];
 then
 	out_str+=""
 	update_count=0
 # only match the full `linux` package, not things like `linux-firmware`
-elif echo "$updates" | grep "^linux " >/dev/null;
+elif echo "$core_updates" | grep "^linux " >/dev/null;
 then
 	urgency="critical"
-	out_str+="k$update_count"
+	out_str+="k$total_update_count"
 else
-	out_str+="p$update_count"
+	out_str+="p$total_update_count"
 fi
 
 echo "$out_str" > $PANEL_FIFO
 
-update_diffs="$(diff -u$MAXLINES $CACHED_UPDATE_LIST <(echo "$updates"))"
+core_update_diffs="$(diff -u$MAXLINES $CACHED_CORE_UPDATE_LIST <(echo "$core_updates"))"
 # remove the diff headers
-update_diffs="$(echo "$update_diffs" | tail -n+4)"
+core_update_diffs="$(echo "$core_update_diffs" | tail -n+4)"
 # ignore any removed entries
-update_diffs="$(echo "$update_diffs" | grep --invert-match '^-')"
+core_update_diffs="$(echo "$core_update_diffs" | grep --invert-match '^-')"
 # remove the leading space from our diff
-update_diffs="$(echo "$update_diffs" | sed 's/^ \(.*\)$/\1/')"
+core_update_diffs="$(echo "$core_update_diffs" | sed 's/^ \(.*\)$/\1/')"
 # make new entries bold
-update_diffs="$(echo "$update_diffs" | sed 's/^+\(.*\)$/<b>\1<\/b>/')"
+core_update_diffs="$(echo "$core_update_diffs" | sed 's/^+\(.*\)$/<b>\1<\/b>/')"
+
+aur_update_diffs="$(diff -u$MAXLINES $CACHED_AUR_UPDATE_LIST <(echo "$aur_updates"))"
+# remove the diff headers
+aur_update_diffs="$(echo "$aur_update_diffs" | tail -n+4)"
+# ignore any removed entries
+aur_update_diffs="$(echo "$aur_update_diffs" | grep --invert-match '^-')"
+# remove the leading space from our diff
+aur_update_diffs="$(echo "$aur_update_diffs" | sed 's/^ \(.*\)$/\1/')"
+# make new entries bold
+aur_update_diffs="$(echo "$aur_update_diffs" | sed 's/^+\(.*\)$/<b>\1<\/b>/')"
 
 # if we have specific diffs, display them, otherwise just list all the updates
-if [[ -n "$update_diffs" ]];
+updates_out=""
+if [[ -n "$core_update_diffs" ]];
 then
-	updates_out="$update_diffs"
+	updates_out+="$core_update_diffs"
 else
-	updates_out="$(cat $CACHED_UPDATE_LIST)"
+	updates_out+="$(cat $CACHED_CORE_UPDATE_LIST)"
+fi
+
+[[ -n "$updates_out" ]] && updates_out+="\n\n"
+
+if [[ -n "$aur_update_diffs" ]];
+then
+	updates_out+="$aur_update_diffs"
+else
+	updates_out+="$(cat $CACHED_AUR_UPDATE_LIST)"
 fi
 
 if [[ "$1" == "notify" ]];
 then
-	notify-send -u "$urgency" "$update_count Updates" "$updates_out"
+	notify-send -u "$urgency" "${core_update_count}+${aur_update_count} Updates" "$updates_out"
 	# update our cached updates list
 	# note that we only need to do this when we're displaying it to the user; we
 	# want to show the most changes between updates, which can be best achieved
 	# by only updating our cached list if we're displaying it to the user
-	echo "$updates" > $CACHED_UPDATE_LIST
+	echo "$core_updates" > $CACHED_CORE_UPDATE_LIST
+	echo "$aur_updates" > $CACHED_AUR_UPDATE_LIST
 fi
 
