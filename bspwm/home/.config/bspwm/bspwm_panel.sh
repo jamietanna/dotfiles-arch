82ab66a6f39b2d7b44d6c84652bf428510434567
Jamie Tanna <jamie@jamietanna.co.uk>
2017-04-16 23:04:49 +0100

    Make Lemonbar font smaller
    There's no point having it super big - it's wasting space! Precious 3K
    screen space can only fit so much /s

--- bspwm/home/.config/bspwm/bspwm_panel.sh
+++ bspwm/home/.config/bspwm/bspwm_panel.sh
@@ -4,7 +4,7 @@ export PATH="$PATH:$HOME/bin"
 
 export PANEL_FIFO=/tmp/panel-fifo
 export PANEL_HEIGHT=24
-export PANEL_FONT_FAMILY="LiberationMono:size=11"
+export PANEL_FONT_FAMILY="LiberationMono:size=08"
 export PANEL_WM_NAME="bspwm_panel"
 export PANEL_UNDERLINE_HEIGHT=3
 
