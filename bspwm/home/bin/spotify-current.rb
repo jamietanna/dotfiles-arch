3e7f572a7c632a443a5258bc6b492b965e3044db
Jamie Tanna <jamie@jamietanna.co.uk>
2018-07-06 18:36:28 +0100

    Migrate to Spotify for music
    I've finally moved to the official Spotify application as I've found
    that offline support is something I'd really like.
    
    We also need to move to `playerctl` which can interface with Spotify.
    
    We need to create a new `spotify-current.rb` script to be called by
    Polybar as it doesn't support it natively.

---
+++ bspwm/home/bin/spotify-current.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+require 'json'
+
+status = `playerctl status --player=spotify 2>/dev/null`.chomp
+
+icon = ''
+
+icon = case status
+       when 'Playing'
+         ''
+       when 'Paused'
+         ''
+       else
+         ''
+       end
+
+puts "#{icon} #{`playerctl metadata xesam:title 2>/dev/null`}"
