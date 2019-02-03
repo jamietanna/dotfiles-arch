c5ef35497a6fed81b7f1e3c96cd7c6e08975d794
Jamie Tanna <jamie@jamietanna.co.uk>
2019-02-03 15:39:34 +0100

    Use Ruby wrapper for battery indicators
    It's getting annoying that the Bash version was having issues comparing
    numbers, so to save some complexity, we'll refactor it out into Ruby.
    
    This unfortunately does mean that there's a Ruby dependency here, which
    means out-of-the-box it may not always work.
    
    This does also require a somewhat hacky way to interface with the
    `.theme_colours.h` file, instead of the generated XResources DB (so we
    have to parse that file, and if we change its setup, it'll break).

---
+++ bspwm/home/bin/bat.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+def extract_xresources_field(colour_name)
+  File.open("#{Dir.home}/.theme_colours.h") do |file|
+    l = file.find do |line|
+      line =~ /^#define #{colour_name}\w+/
+    end
+    l.split('#')[2].chomp
+  end
+end
+
+DATA = `acpi -b`.freeze
+remainder = DATA.match(/([0-9]{2}):([0-9]{2}):([0-9]{2})/)
+percentage = DATA.match(/([0-9]+)%/)[0]
+
+background = ''
+foreground = ''
+
+output = ''
+charging_indicator = ''
+
+if DATA.match?(/Discharging/)
+  hours = remainder[0].to_i
+  mins = remainder[1].to_i
+  secs = remainder[2].to_i
+
+  if percentage.to_i <= 10 ||
+      hours == 0 && mins <= 30
+    background = extract_xresources_field 'col_red'
+    foreground = extract_xresources_field 'col_white'
+  end
+
+  if percentage.to_i <= 30 ||
+      hours == 0
+    foreground = extract_xresources_field 'col_red'
+  end
+
+else
+  charging_indicator = '+'
+end
+
+output = "%{B#{background}}" +
+  "%{F#{foreground}}" +
+  percentage +
+  ' ' +
+  charging_indicator +
+  remainder[0] +
+  '%{F-}%{B-}'
+
+puts output
