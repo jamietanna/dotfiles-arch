ff349eb82874e6d582fd59ff0deb7117dc522005
Jamie Tanna <jamie@jamietanna.co.uk>
2021-12-21 09:31:50 +0000

    Artist not album artist

--- bspwm/home/bin/spotify-current.rb
+++ bspwm/home/bin/spotify-current.rb
@@ -14,4 +14,4 @@ icon = case status
          ''
        end
 
-puts "#{icon} #{`playerctl metadata xesam:albumArtist 2>/dev/null`.delete "\n"} - #{`playerctl metadata xesam:title 2>/dev/null`.delete "\n"}"
+puts "#{icon} #{`playerctl metadata xesam:artist 2>/dev/null`.delete "\n"} - #{`playerctl metadata xesam:title 2>/dev/null`.delete "\n"}"
