b14db2995d8c230f2b67d59ee7af4f9d9fb9e371
Jamie Tanna <jamie@jamietanna.co.uk>
2022-04-08 10:36:47 +0100

    Add `shorten` CLI for Bitly shortlink creation
    Maritally taken from an OpenAPI generated set of models, with
    oapi-codegen, but unfortunately not completely through that due to [0].
    
    [0]: https://github.com/deepmap/oapi-codegen/issues/549

---
+++ go/home/go/src/jvt.me/dotfiles/shorten/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type Shorten struct {
+	LongUrl string `json:"long_url"`
+}
+
+type BitlinkUpdate struct {
+	Archived       *bool     `json:"archived,omitempty"`
+	ClientId       *string   `json:"client_id,omitempty"`
+	CreatedAt      *string   `json:"created_at,omitempty"`
+	CreatedBy      *string   `json:"created_by,omitempty"`
+	CustomBitlinks *[]string `json:"custom_bitlinks,omitempty"`
+	Id             *string   `json:"id,omitempty"`
+	IsDeleted      *bool     `json:"is_deleted,omitempty"`
+	LaunchpadIds   *[]string `json:"launchpad_ids,omitempty"`
+	Link           *string   `json:"link,omitempty"`
+	LongUrl        *string   `json:"long_url,omitempty"`
+	Tags           *[]string `json:"tags,omitempty"`
+	Title          *string   `json:"title,omitempty"`
+}
+
+const (
+	API_URL = "https://api-ssl.bitly.com/v4/shorten"
+)
+
+func ArgfRead() (string, error) {
+	var bytes []byte
+	var err error
+
+	if len(os.Args) >= 2 {
+		bytes, err = os.ReadFile(os.Args[1])
+	} else {
+		bytes, err = io.ReadAll(os.Stdin)
+	}
+
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSuffix(string(bytes), "\n"), nil
+}
+
+func main() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath("$HOME/.config/bitly")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+	if !viper.IsSet("access_token") {
+		panic(fmt.Errorf("No access_token set"))
+	}
+
+	url, err := ArgfRead()
+	if err != nil {
+		panic(err)
+	}
+	shortenBody := Shorten{
+		LongUrl: url,
+	}
+	bytes, err := json.Marshal(shortenBody)
+	if err != nil {
+		panic(err)
+	}
+
+	payload := strings.NewReader(string(bytes))
+
+	req, _ := http.NewRequest("POST", API_URL, payload)
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+viper.GetString("access_token"))
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	if 200 != res.StatusCode {
+		panic(fmt.Errorf("HTTP %d returned, with body: %s", res.StatusCode, string(body)))
+	}
+
+	var response BitlinkUpdate
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(*response.Link)
+}
