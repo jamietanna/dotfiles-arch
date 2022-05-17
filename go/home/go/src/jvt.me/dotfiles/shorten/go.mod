5949d32207965e32ada1c236948a4a7ad562eea2
Jamie Tanna <jamie@jamietanna.co.uk>
2022-05-17 16:21:57 +0100

    Migrate to generated OpenAPI client for Bitly
    Instead of my custom code, we can instead use the generated OpenAPI
    client from `oapi-codegen`, which makes things much simpler.

--- go/home/go/src/jvt.me/dotfiles/shorten/go.mod
+++ go/home/go/src/jvt.me/dotfiles/shorten/go.mod
@@ -4,24 +4,14 @@ go 1.18
 
 require (
 	github.com/deepmap/oapi-codegen v1.10.1
-	github.com/getkin/kin-openapi v0.94.0
-	github.com/labstack/echo/v4 v4.7.2
 	github.com/spf13/viper v1.10.1
 )
 
 require (
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
-	github.com/ghodss/yaml v1.0.0 // indirect
-	github.com/go-openapi/jsonpointer v0.19.5 // indirect
-	github.com/go-openapi/swag v0.21.1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/josharian/intern v1.0.0 // indirect
-	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
-	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
@@ -29,10 +19,6 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
-	golang.org/x/net v0.0.0-20220418201149-a630d4f3e7a2 // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
--- go/home/go/src/jvt.me/dotfiles/shorten/main.go
+++ go/home/go/src/jvt.me/dotfiles/shorten/main.go
@@ -1,21 +1,20 @@
-//go:generate oapi-codegen -generate types --package=main  -o ./bitly.gen.go https://dev.bitly.com/v4/v4.json
+//go:generate oapi-codegen -generate types,client --package=main  -o ./bitly.gen.go https://dev.bitly.com/v4/v4.json
 
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
 	"io"
-	"io/ioutil"
-	"net/http"
 	"os"
 	"strings"
 
+	"github.com/deepmap/oapi-codegen/pkg/securityprovider"
 	"github.com/spf13/viper"
 )
 
 const (
-	API_URL = "https://api-ssl.bitly.com/v4/shorten"
+	API_URL = "https://api-ssl.bitly.com/v4"
 )
 
 func ArgfRead() (string, error) {
@@ -45,46 +44,32 @@ func main() {
 	if !viper.IsSet("access_token") {
 		panic(fmt.Errorf("No access_token set"))
 	}
+	bearerTokenProvider, err := securityprovider.NewSecurityProviderBearerToken(viper.GetString("access_token"))
+	if err != nil {
+		panic(err)
+	}
 
 	url, err := ArgfRead()
 	if err != nil {
 		panic(err)
 	}
-	shortenBody := Shorten{
+	shortenBody := CreateBitlinkJSONRequestBody{
 		LongUrl: url,
 	}
-	bytes, err := json.Marshal(shortenBody)
+
+	client, err := NewClientWithResponses(API_URL, WithRequestEditorFn(bearerTokenProvider.Intercept))
 	if err != nil {
 		panic(err)
 	}
 
-	payload := strings.NewReader(string(bytes))
-
-	req, _ := http.NewRequest("POST", API_URL, payload)
-
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", "Bearer "+viper.GetString("access_token"))
-
-	res, err := http.DefaultClient.Do(req)
+	req, err := client.CreateBitlinkWithResponse(context.Background(), shortenBody)
 	if err != nil {
 		panic(err)
 	}
 
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		panic(err)
+	if req.StatusCode() != 200 {
+		panic(fmt.Errorf("failed to create Bitlink: HTTP %d", req.StatusCode()))
 	}
 
-	if 200 != res.StatusCode {
-		panic(fmt.Errorf("HTTP %d returned, with body: %s", res.StatusCode, string(body)))
-	}
-
-	var response BitlinkUpdate
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(*response.Link)
+	fmt.Println(*req.JSON200.Link)
 }
