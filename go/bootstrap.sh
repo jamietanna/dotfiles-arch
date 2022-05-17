09882664f70a4bfe80463252a440279c3c38e58e
Jamie Tanna <jamie@jamietanna.co.uk>
2022-05-17 15:48:20 +0100

    Fix: correct path references for building Go utilities

--- go/bootstrap.sh
+++ go/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 cwd="$(dirname "${BASH_SOURCE[0]}")"
 
-for dir in home/go/src/jvt.me/dotfiles/*; do
+for dir in "$cwd"/home/go/src/jvt.me/dotfiles/*; do
   (
-    cd "$cwd/$dir" || exit 1
+    cd "$dir" || exit 1
     go install
   )
 done
